c5ed0abb68a696a0cbf3efb86225e0da1b57c8cc
jatin_chauhan123 <135827076+Jatin-chauhan-beep@users.noreply.github.com>
2024-09-02 12:00:57 +0530

    code till 02-09-2024

--- src/view/SuperAdmin/Dispatch/Foreign/NewDispatchForm/components/ItemList/NewItemDialog.js
+++ src/view/SuperAdmin/Dispatch/Foreign/NewDispatchForm/components/ItemList/NewItemDialog.js
@@ -1,46 +1,52 @@
-import React, { memo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Dialog } from '../../../../../../../components/ui'
-import ItemForm from './ItemForm';
-import { toggleAddDispatchItemDialog } from '../../../NewDispatch/store/stateSlice';
-
+import React, { memo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Dialog } from "../../../../../../../components/ui";
+import ItemForm from "./ItemForm";
+import { toggleAddDispatchItemDialog } from "../../../NewDispatch/store/stateSlice";
 
 const NewDispatchItemDialog = (props) => {
-    const { boxes = [], locationIndex, setFieldValue, dispatchList = [], addNewItemInPoList } = props
-    const dispatch = useDispatch()
+  const {
+    boxes = [],
+    locationIndex,
+    setFieldValue,
+    dispatchList = [],
+    addNewItemInPoList,
+  } = props;
+  const dispatch = useDispatch();
 
-    const addDispatchItemDialog = useSelector(
-        (state) => state.new_foreign_invoice.state.addDispatchItemDialog
-    )
+  const addDispatchItemDialog = useSelector(
+    (state) => state.new_foreign_invoice.state.addDispatchItemDialog
+  );
 
-    const onDialogClose = () => {
-        dispatch(toggleAddDispatchItemDialog({ option: false, locationIndex: null }))
-    }
+  const onDialogClose = () => {
+    dispatch(
+      toggleAddDispatchItemDialog({ option: false, locationIndex: null })
+    );
+  };
 
+  const handleNewItem = (values) => {
+    addNewItemInPoList?.(dispatchList, values, locationIndex, setFieldValue);
+    onDialogClose();
+  };
 
-    const handleNewItem = (values) => {
-        addNewItemInPoList?.(dispatchList, values, locationIndex, setFieldValue)
-        // onDialogClose()
-    }
+  return (
+    <Dialog
+      isOpen={
+        addDispatchItemDialog.option &&
+        locationIndex === addDispatchItemDialog.locationIndex
+      }
+      onClose={onDialogClose}
+      onRequestClose={onDialogClose}
+    >
+      <ItemForm
+        boxes={boxes}
+        type="new"
+        dispatchList={dispatchList}
+        handleFormSubmit={handleNewItem}
+        onDiscard={onDialogClose}
+      />
+    </Dialog>
+  );
+};
 
-
-    return (
-        <Dialog
-            isOpen={addDispatchItemDialog.option && locationIndex === addDispatchItemDialog.locationIndex}
-            onClose={onDialogClose}
-            onRequestClose={onDialogClose}
-        >
-            <ItemForm
-                boxes={boxes}
-                type='new'
-                dispatchList={dispatchList}
-                handleFormSubmit={handleNewItem}
-                onDiscard={onDialogClose}
-            />
-        </Dialog >
-    )
-}
-
-
-
-export default memo(NewDispatchItemDialog)
+export default memo(NewDispatchItemDialog);
