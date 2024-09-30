efc08360224e6f4c30adae5b29f994cf6a40047a
jatin_chauhan123 <135827076+Jatin-chauhan-beep@users.noreply.github.com>
2024-09-30 14:44:35 +0530

    user bugs fixed

--- src/services/SuperAdmin/UserService.js
+++ src/services/SuperAdmin/UserService.js
@@ -1,34 +1,32 @@
-import ApiService from "../ApiService";
+import ApiService from '../ApiService'
 
 export async function apiGetAllUserWithPagination(data) {
-    return ApiService.fetchData({
-        url: 'v1/web/company/user',
-        method: 'post',
-        data,
-    })
+  return ApiService.fetchData({
+    url: 'v1/web/company/user',
+    method: 'post',
+    data
+  })
 }
 
-
 export async function apiPostNewUserRegister(data) {
-    return ApiService.fetchData({
-        url: 'v1/web/company/user/register',
-        method: 'post',
-        data,
-    })
+  return ApiService.fetchData({
+    url: 'v1/web/company/user/register',
+    method: 'post',
+    data
+  })
 }
 
 export async function apiUpdateUserDetails(data) {
-    return ApiService.fetchData({
-        url: 'v1/web/company/user/update',
-        method: 'put',
-        data,
-    })
+  return ApiService.fetchData({
+    url: 'v1/web/company/user/update/id',
+    method: 'put',
+    data
+  })
 }
 export async function apiDeleteUser(data) {
-    return ApiService.fetchData({
-        url: 'v1/web/company/user/delete',
-        method: 'delete',
-        data,
-    })
+  return ApiService.fetchData({
+    url: 'v1/web/company/user/delete/id',
+    method: 'delete',
+    data
+  })
 }
-
--- src/view/SuperAdmin/User/components/UserDeleteConfirmation.js
+++ src/view/SuperAdmin/User/components/UserDeleteConfirmation.js
@@ -7,68 +7,78 @@ import { getAllUsers } from '../store/dataSlice'
 import { apiDeleteUser } from '../../../../services/SuperAdmin/UserService'
 
 const UserDeleteConfirmation = () => {
-    const [loading, setLoading] = useState(false)
-    const dispatch = useDispatch()
-    const selectedUser = useSelector(
-        (state) => state.user.state.selectedUser
-    )
-    const deleteUserDialog = useSelector(
-        (state) => state.user.state.deleteUserDialog
-    )
-    const tableData = useSelector(
-        (state) => state.user.data.tableData
-    )
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  const selectedUser = useSelector((state) => state.user.state.selectedUser)
+  const deleteUserDialog = useSelector(
+    (state) => state.user.state.deleteUserDialog
+  )
+  const tableData = useSelector((state) => state.user.data.tableData)
 
-    const onDialogClose = () => {
-        dispatch(toggleDeleteUserDialog(false))
-    }
+  const onDialogClose = () => {
+    dispatch(toggleDeleteUserDialog(false))
+  }
 
-    const onDelete = async () => {
-        setLoading(true)
-        const response = await apiDeleteUser(selectedUser)
-        if (response.data?.success) {
-            deleteSucceed(true)
-        }
-        setLoading(false)
-    }
-
-    const deleteSucceed = (success) => {
-        if (success) {
-            onDialogClose()
-            dispatch(getAllUsers(tableData))
-            Toast.push(
-                <Notification
-                    title={'Successfully Deleted'}
-                    type="success"
-                    duration={2500}
-                >
-                    User successfully deleted
-                </Notification>,
-                {
-                    placement: 'top-center',
-                }
-            )
-        }
-    }
-
-    return (
-        <ConfirmDialog
-            isOpen={deleteUserDialog}
-            onClose={onDialogClose}
-            onRequestClose={onDialogClose}
-            type="danger"
-            title="Delete User"
-            onCancel={onDialogClose}
-            onConfirm={onDelete}
-            confirmText={!loading ? 'Confirm' : 'Deleting'}
-            confirmButtonColor="red-600"
+  const onDelete = async () => {
+    setLoading(true)
+    try {
+      const response = await apiDeleteUser({ user_id: selectedUser.user_id })
+      if (response.data?.success) {
+        deleteSucceed(true)
+      }
+    } catch (error) {
+      Toast.push(
+        <Notification
+          title={'Error'}
+          type="danger"
+          duration={2500}
         >
-            <p>
-                Are you sure you want to delete this user? This action cannot be
-                undone.
-            </p>
-        </ConfirmDialog>
-    )
+          User Not deleted
+        </Notification>,
+        {
+          placement: 'top-center'
+        }
+      )
+    }
+    setLoading(false)
+  }
+
+  const deleteSucceed = (success) => {
+    if (success) {
+      onDialogClose()
+      dispatch(getAllUsers(tableData))
+      Toast.push(
+        <Notification
+          title={'Successfully Deleted'}
+          type="success"
+          duration={2500}
+        >
+          User successfully deleted
+        </Notification>,
+        {
+          placement: 'top-center'
+        }
+      )
+    }
+  }
+
+  return (
+    <ConfirmDialog
+      isOpen={deleteUserDialog}
+      onClose={onDialogClose}
+      onRequestClose={onDialogClose}
+      type="danger"
+      title="Delete User"
+      onCancel={onDialogClose}
+      onConfirm={onDelete}
+      confirmText={!loading ? 'Confirm' : 'Deleting'}
+      confirmButtonColor="red-600"
+    >
+      <p>
+        Are you sure you want to delete this user? This action cannot be undone.
+      </p>
+    </ConfirmDialog>
+  )
 }
 
 export default UserDeleteConfirmation
