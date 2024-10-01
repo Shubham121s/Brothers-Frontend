442a74cd4936f26ec039c321aa198e9e1be81344
jatin_chauhan123 <135827076+Jatin-chauhan-beep@users.noreply.github.com>
2024-10-01 17:27:03 +0530

    dashboard Changes

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,7 +1,7 @@
 const appConfig = {
-  apiPrefix: 'https://api-erp.brothers.net.in/api/',
+  // apiPrefix: 'https://api-erp.brothers.net.in/api/',
   // apiPrefix: 'https://erp.brothers.5techg.com/api/',
-  // apiPrefix: 'http://localhost:5005/api/',
+  apiPrefix: 'http://localhost:5005/api/',
   authenticatedEntryPath: '/dashboard',
   unAuthenticatedEntryPath: '/sign-in',
   tourPath: '/',
--- src/view/SuperAdmin/Dashboard/components/Statistic.js
+++ src/view/SuperAdmin/Dashboard/components/Statistic.js
@@ -3,47 +3,59 @@ import { Card } from '../../../../components/ui'
 import { NumericFormat } from 'react-number-format'
 
 const StatisticCard = ({ data = 0, label, valuePrefix, className }) => {
-    return (
-        <Card className={`h-max ${className}`}>
-            <h4 className="font-semibold mb-2">{label}</h4>
-            <div className="flex justify-between items-center">
-                <div>
-                    <h3 className="font-bold">
-                        <NumericFormat
-                            displayType="text"
-                            value={data}
-                            thousandSeparator
-                            prefix={valuePrefix}
-                        />
-                    </h3>
-                </div>
-            </div>
-        </Card>
-    )
+  return (
+    <Card className={`h-max ${className}`}>
+      <h4 className="font-semibold mb-2">{label}</h4>
+      <div className="flex justify-between items-center">
+        <div>
+          <h3 className="font-bold">
+            <NumericFormat
+              displayType="text"
+              value={data}
+              thousandSeparator
+              prefix={valuePrefix}
+            />
+          </h3>
+        </div>
+      </div>
+    </Card>
+  )
 }
 
 const Statistic = ({ data = {} }) => {
-    return (
-        <div className="grid grid-cols-2 gap-4 h-max">
-            <StatisticCard
-                data={data.totalInvoice}
-                valuePrefix="₹"
-                label="Revenue"
-                className='bg-pink-50'
-            />
-            <StatisticCard
-                data={data.totalInvoice}
-                label="Orders"
-                className='bg-emerald-50'
-            />
-            <StatisticCard
-                data={0}
-                valuePrefix="₹"
-                label="Purchases"
-                className='bg-orange-50'
-            />
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-2 gap-4 h-max">
+      <StatisticCard
+        data={data.revenue}
+        valuePrefix="₹"
+        label="Revenue"
+        className="bg-pink-50"
+      />
+      <StatisticCard
+        data={data.TotalOrders}
+        label="Orders"
+        className="bg-emerald-50"
+      />
+      <StatisticCard
+        data={data.totalInvoice}
+        valuePrefix="₹"
+        label="Invoices"
+        className="bg-rose-50"
+      />
+      <StatisticCard
+        data={data.Purchases}
+        valuePrefix="₹"
+        label="Purchases"
+        className="bg-orange-50"
+      />
+      <StatisticCard
+        data={data.pendingPOs}
+        valuePrefix="₹"
+        label="Pending PO's"
+        className="bg-rose-50"
+      />
+    </div>
+  )
 }
 
 export default Statistic
